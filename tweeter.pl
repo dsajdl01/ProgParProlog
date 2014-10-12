c2b148fe398747dbda384f15066ff4ce5607c720
dsajdl01 <davidsajdl@yahoo.co.uk>
2014-10-12 12:40:48 +0100

    adding tweeter pl file

---
+++ tweeter.pl
@@ -0,0 +1,19 @@
+followed(anne,	fred).
+followed(fred, 	julia).
+followed(fred, 	susan).
+followed(john, 	fred).
+followed(julia, fred).
+followed(susan,	john).
+followed(susan,	julia).
+
+tweeted(anne,	tweet1).
+tweeted(anna,	tweet5).
+tweeted(fred, 	tweet2).
+tweeted(fred, 	tweet7).
+tweeted(fred, 	tweet8).
+tweeted(john, 	tweet3).
+tweeted(john, 	tweet4).
+tweeted(julia, 	tweet6).
+tweeted(susan, 	tweet9).
+tweeted(susan, 	tweet10).
+
