d58b32df69bfe5a09f86a0195b8d1bce4e521fbe
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-05-21 16:16:00 +0100

    more exercirses

--- invented.pl
+++ invented.pl
@@ -17,13 +17,20 @@ no_beam :-
 		genius(P),
 		write('I found intelligent life, sir!'), nl.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  http:///www.learnprolognow.org/lpnpage.php?pagetype=html&pageid=lpn-htmlse16  %%
+
+
+%% Exercise 4.3
 second(_,[]).
 second(X,[_,S|_]) :- (X = S).
 
+
+%% Exercise 4.4
 swop12([X,X1|Tail], [Y,Y1| Tail1]):- (Y = X1), 
 									(Y1 = X), 
 									(Tail = Tail1).
-
+%% Exercise 4.5
 tran(eins, one).
 tran(zwei, two).
 tran(drei, three).
@@ -34,7 +41,6 @@ tran(sieben, seven).
 tran(acht, eight).
 tran(neun, nine).
 
-
 listtran([],[]).
 listtran(G, [E|Tail]):- listtran(Add, Tail),
 						tran(GmNum, E),
@@ -43,4 +49,10 @@ listtran(G, [E|Tail]):- listtran(Add, Tail),
 listtran([],[]).
 listtran([G|Tail],E):- listtran(Tail, Add),
 						tran(G, EnNum),
-						append([EnNum], Add, E),!. 
+						append([EnNum], Add, E),!.
+
+%% Exercise 4.6
+twice([],[]).
+twice([H|T],Result):-
+			twice(T, Rest),
+			append([H,H], Rest, Result).
