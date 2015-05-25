f39c8cfa60375769e24362177f4c3bd952867fcf
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-05-25 13:03:29 +0100

    more exercises to create predicates include results

--- moreExamples.pl
+++ moreExamples.pl
@@ -67,6 +67,62 @@ suffix(A, B) :- myAppend(_, A, B).
 %						;=>  false.
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 
+% write predicete which give a position integer of N, and returns a list or sequence of the integers N inclusive through 1 inclusive.
+% e.g. getList(5,[5,4,3,2,1]) 
+
 getlist(0, []).
-getlist(X,[X|List]) :- Prev is X - 1, getlist(Prev, List).
+getlist(X,[X|List]) :- Prev is X - 1, getlist(Prev, List),!.
+
+getListRev(X, List) :- getmyList(X, 1, List).
+
+getmyList(X, X , [X]).
+getmyList(X, Y, [Y|List]) :- (Y < X),
+							NewY is Y + 1,
+							getmyList(X, NewY, List),!.							
+
+% Result 
+% ?- getlist(5,X). 			;=>  X = [5, 4, 3, 2, 1] .
+% ?- getlist(10,X). 		;=>  X = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1] 
+%
+% ?- getListRev(4,X). 		;=>  X = [1, 2, 3, 4].
+% ?- getListRev(12,X). 		;=>  X = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].
+%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% write predicate which give 2 integers Y X and returns a list of integers between Y and Y inclusive
+% It should work both way if the X is smaller than Y then the list produce integers from X to Y e.g. numbet(1,4,[1,2,3,4]).
+% if the X is greater than Y then the list produce integers from X to Y e.g. numbet(4,1,[4,3,2,1]). 
+
+numbertween(X, X, [X]).
+numbertween(X, Y, [X|List]) :- (X < Y),
+						NewX is X + 1,
+						numbertween(NewX,Y, List).
+numbertween(X, X, [X]).
+numbertween(X, Y, [X|List]) :- (X > Y),
+						NewX is X - 1,
+						numbertween(NewX, Y, List).
+
+% Result;
+% ?- numbertween(5,11,X). 		;=>  X = [5, 6, 7, 8, 9, 10, 11] .
+% ?- numbertween(-2,2,X). 		:=>  X = [-2, -1, 0, 1, 2] .
+% ?- numbertween(3,-2,X). 		;=>  X = [3, 2, 1, 0, -1, -2] .
+% ?- numbertween(58,49,X). 		;=>  X = [58, 57, 56, 55, 54, 53, 52, 51, 50, 49] .
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% write predicete which get integer and returns fibonacci number of the paricular integer, e.g f(3,X) ;=> X = 2
+
+fib(0,0).
+fib(1,1).
+fib(N,F) :- N1 is N - 1,
+			N2 is N - 2,
+			fib(N1, F1),
+			fib(N2, F2),
+			F is F1 + F2,!.
+
+% Result
+% ?- fib(12,X). 	;=>  X = 144.
+% ?- fib(7,X). 		;=>  X = 13.
+% ?- fib(3,X). 		;=>  X = 2.
+% ?- fib(1,X). 		;=>  X = 1 
+
+
